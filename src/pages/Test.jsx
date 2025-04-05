877747f5fe288aa56e0cee9d76071e9a5da622b4
kevinquique12 <kevn.1995.cod@gmail.com>
2025-04-05 09:04:28 -0500

    Test page and supabase connection

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,13 +1,30 @@
 
+import { useEffect } from "react";
+import { supabase } from "../supabase";
 
 // Pagina de pruebas
 function Test() {
-  
-  return (
-    <>
-        <h1>Hola mundo</h1>    
-    </>
-  )
+
+    useEffect(() => {
+        getMatrizModelo();
+    }, [])
+
+    return (
+        <>
+            <h1>Hola mundo</h1>
+        </>
+    )
+
+    // Obtener matriz modelo
+    async function getMatrizModelo() {
+        try {
+            const { data, error } = await supabase.from("matriz_modelo_duplicate").select();
+            if (error) throw error;
+            console.log(data);
+        } catch (error) {
+            console.error("Error fetching matriz modelo:", error);
+        }
+    }
 }
 
 export { Test }
