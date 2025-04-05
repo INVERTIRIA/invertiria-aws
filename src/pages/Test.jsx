9810a6ee5dbc3209139f815b329008ecaa01f7e1
kevinquique12 <kevn.1995.cod@gmail.com>
2025-04-05 09:19:05 -0500

    Test page and supbase connection

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -20,6 +20,10 @@ function Test() {
             const { data, error } = await supabase.from("matriz_modelo_duplicate").select();
             if (error) throw error;
             console.log(data);
+
+            console.log('primer elemento de la matriz:', data[0].matriz[0][0])
+            console.log('primer elemento de la matriz jsonb:', data[0].matriz_2[0][0])
+
         } catch (error) {
             console.error("Error fetching matriz modelo:", error);
         }
--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const PROJECT_URL = import.meta.env.VITE_PROJECT_URL
-const ANON_KEY = import.meta.env.VITE_ANON_KEY
+const PROJECT_URL = import.meta.env.VITE_SUPABASE_PROJECT_URL
+const ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(PROJECT_URL, ANON_KEY);
