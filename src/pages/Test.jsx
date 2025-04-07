c8191913a057fd5824a370d3c5d889b56d1348e2
kevinquique12 <kevn.1995.cod@gmail.com>
2025-04-07 17:39:45 -0500

    Reconnection

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -17,13 +17,9 @@ function Test() {
     // Obtener matriz modelo
     async function getMatrizModelo() {
         try {
-            const { data, error } = await supabase.from("matriz_modelo_duplicate").select();
+            const { data, error } = await supabase.from("matriz_modelo").select();
             if (error) throw error;
             console.log(data);
-
-            console.log('primer elemento de la matriz:', data[0].matriz[0][0])
-            console.log('primer elemento de la matriz jsonb:', data[0].matriz_2[0][0])
-
         } catch (error) {
             console.error("Error fetching matriz modelo:", error);
         }
